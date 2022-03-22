496d9e6dceaf40af53a9e41b86405df36c57ed75
rushikesh-ladke <rushikesh.ladke@invimatic.in>
2022-03-22 06:49:05 +0530

    :package: antd and react query added

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import { App , Login } from './components';
+import { App , Login } from 'components';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
