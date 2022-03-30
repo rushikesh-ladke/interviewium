c513987e5bb4272632c2b97eddf6ce3bf43e13e4
Sonam Diwate <sonam.diwate@itwox.com>
2022-03-30 19:59:20 +0530

    sidebar changes

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,8 +1,10 @@
 import { Login } from 'components/Login';
 import { Register } from 'components/Register';
+import { Sidebar } from 'components/Sidebar';
 export const App = () => {
   return (<>
     {/* <Login /> */}
-    <Register />
+    {/* <Register /> */}
+    <Sidebar />
   </>);
 }
