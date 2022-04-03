0f96164a493e8b28793d9c5347781dd66deec487
rushikesh-ladke <rushikesh.ladke@invimatic.in>
2022-04-03 16:11:13 +0530

    :package: added firebase package and initial code for login

--- src/components/Login/login-api.tsx
+++ src/components/Login/login-api.tsx
@@ -1,1 +1,6 @@
-export {}
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import { auth } from 'shared/firebase-config';
+
+export function signIn(email: string, password: string) {
+  return signInWithEmailAndPassword(auth, email, password);
+}
