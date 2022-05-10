a3cdb70b6bab266270bcfa9c3ecf02a74f1633ec
rushikesh-ladke <rushikesh.ladke@invimatic.in>
2022-05-10 18:32:06 +0530

    :ambulance: Modal issue resolved

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -4,10 +4,13 @@ import styles from './styles.module.scss';
 import Logo from '../../images/Interviewiumlogo.svg';
 import HomeBanner from '../../images/homebanner.svg';
 export const Home = () => {
+
   const [isModalVisible, setIsModalVisible] = useState(false);
 
+  const ModalAntd: any = Modal;
+
   const showModal = () => {
-    setIsModalVisible(true);
+    setIsModalVisible(!isModalVisible);
   };
 
   const handleOk = () => {
@@ -17,59 +20,85 @@ export const Home = () => {
   const handleCancel = () => {
     setIsModalVisible(false);
   };
-  return (<>
-    <div className={`${styles.HomepageMain} container`}>
-      <header className={styles.header}>
-        <img className={styles.logo} src={Logo} alt="Logo" />
-        <button className={styles.contactBtn}>Contact</button>
-      </header>
-      <section className={styles.bannerSection}>
-        <div className="row">
-          <div className="col-md-6 d-flex flex-column justify-content-center">
-            <h1>Let's start<br /> something<br /> With us.</h1>
-            <button className={styles.getStartedBtn} onClick={showModal}>Get Started</button>
-            {/* <Modal text title="Basic Modal" visible={isModalVisible} onOk={handleOk} onCancel={handleCancel}>
-              <p>Some contents...</p>
-              <p>Some contents...</p>
-              <p>Some contents...</p>
-            </Modal> */}
+  return (
+    <>
+      <div className={`${styles.HomepageMain} container`}>
+        <header className={styles.header}>
+          <img className={styles.logo} src={Logo} alt='Logo' />
+          <button className={styles.contactBtn} onClick={() => showModal()}>
+            Contact
+          </button>
+        </header>
+        <section className={styles.bannerSection}>
+          <div className='row'>
+            <div className='col-md-6 d-flex flex-column justify-content-center'>
+              <h1>
+                Let's start
+                <br /> something
+                <br /> With us.
+              </h1>
+              <button className={styles.getStartedBtn} onClick={showModal}>
+                Get Started
+              </button>
+              <ModalAntd
+                text
+                title='Basic Modal'
+                visible={isModalVisible}
+                onOk={() => handleOk()}
+                onCancel={() => handleCancel()}
+              >
+                <p>Some contents...</p>
+                <p>Some contents...</p>
+                <p>Some contents...</p>
+              </ModalAntd>
+            </div>
+            <div className='col-md-6'>
+              <img
+                className={styles.HomeBanner}
+                src={HomeBanner}
+                alt='HomeBanner'
+              />
+            </div>
           </div>
-          <div className="col-md-6">
-            <img className={styles.HomeBanner} src={HomeBanner} alt="HomeBanner" />
-          </div>
-        </div>
-      </section>
-      <section className={styles.bottom_section}>
-        <div className="row">
-          <div className="col-lg-4">
-            <div className={styles.barMain}>
-              <div>
-                <h4>Transfer money</h4>
-                <p>Bootstrap employs a handful of important global styles and settings that you’ll need to
-                  be aware of when using it</p>
+        </section>
+        <section className={styles.bottom_section}>
+          <div className='row'>
+            <div className='col-lg-4'>
+              <div className={styles.barMain}>
+                <div>
+                  <h4>Transfer money</h4>
+                  <p>
+                    Bootstrap employs a handful of important global styles and
+                    settings that you’ll need to be aware of when using it
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className='col-lg-4'>
+              <div className={styles.barMain}>
+                <div>
+                  <h4>Safe Transfer</h4>
+                  <p>
+                    Bootstrap employs a handful of important global styles and
+                    settings that you’ll need to be aware of when using it
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className='col-lg-4'>
+              <div className={styles.barMain}>
+                <div>
+                  <h4>Real Partnership</h4>
+                  <p>
+                    Bootstrap employs a handful of important global styles and
+                    settings that you’ll need to be aware of when using it
+                  </p>
+                </div>
               </div>
             </div>
           </div>
-          <div className="col-lg-4">
-            <div className={styles.barMain}>
-              <div>
-                <h4>Safe Transfer</h4>
-                <p>Bootstrap employs a handful of important global styles and settings that you’ll need to
-                  be aware of when using it</p>
-              </div>
-            </div>
-          </div>
-          <div className="col-lg-4">
-            <div className={styles.barMain}>
-              <div>
-                <h4>Real Partnership</h4>
-                <p>Bootstrap employs a handful of important global styles and settings that you’ll need to
-                  be aware of when using it</p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
-    </div>
-  </>);
+        </section>
+      </div>
+    </>
+  );
 };
